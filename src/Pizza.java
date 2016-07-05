16d7182ea25ff16e84696c4955d7194ab98a7fe2
matheus2712 <matheus.costa5@fatec.sp.gov.br>
2016-07-05 16:52:50 -0300

    COURSERA_Pizzaria
    Projeto semana 2

---
+++ src/Pizza.java
@@ -0,0 +1,42 @@
+
+public class Pizza {
+
+	
+	
+	
+	public Pizza(){
+		
+		
+		
+	}
+	
+	
+	
+	public String  adicionaingrediente(String ingrediente){
+		
+		
+		return "a";
+	}
+	
+	
+	public void getPreco(){
+		
+		if(qtdeingrediente <= 2){
+			valuni += 15;
+		} else
+			 if(qtdeingrediente > 2 && qtdeingrediente <= 5){
+				valuni += 20;
+		}else{
+			valuni += 23;
+		}
+		
+				
+	}
+	
+	
+	public static contabilizaIngrediente() 
+	
+	
+	
+	
+}
